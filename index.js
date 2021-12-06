14cb8540d1084af1d080ff08e57c6c36a5fe7903
unknown <khairahmed234@gmail.com>
2021-12-07 00:29:30 +0600

    api added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// jwt token 
+// import { initializeApp ,getAuth} from 'firebase-admin/app';
+const { initializeApp ,getAuth } = require('firebase-admin/app');
+var admin = require("firebase-admin");
+var serviceAccount = require("./configs/volounter-network-firebase-adminsdk-4abo6-d7b8e6d8a0.json");
+
+
+
+
+
+
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -21,6 +32,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false })) //for from
 
 const port = process.env.PORT || 5600
+
+
+// jwt token 
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -54,21 +72,61 @@ client.connect(err => {
 
   // delete 
 
-  app.delete("/delete/:id",(req , res)=>{
-    productCollection.deleteOne({_id: ObjectId(req.params.id)})
-    .then(res => {
-      console.log(res)
-    })
+  app.delete("/delete/:id", (req, res) => {
+    productCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(res => {
+        console.log(res)
+      })
   })
 
-// find single data info 
-app.get("/event/:id" , (req , res)=>{
-  // console.log(req.params.id);
-  productCollection.find({_id : ObjectId(req.params.id)})
-  .toArray((err , doc)=>{
-    res.send(doc[0])
+  // find single data info 
+  app.get("/event/:id", (req, res) => {
+    // console.log(req.params.id);
+    productCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, doc) => {
+        res.send(doc[0])
+      })
   })
-})
+
+  // authenticate data 
+  app.get("/authenticateData", (req, res) => {
+    const bearer = req.headers.authorization
+    const queryEmail = req.query.email
+    if (bearer && bearer.startsWith("Bearer ")) {
+      const idToken = bearer.split(" ")[1]
+      admin.auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const jwtTokenEmail = decodedToken.email
+          // console.log(jwtTokenEmail , queryEmail)
+          // console.log(jwtTokenEmail === queryEmail)
+          if(queryEmail === jwtTokenEmail){
+            productCollection.find({email : req.query.email})
+            .toArray((err , doc)=>{
+              res.send(doc)
+            })
+          }
+          else{
+            res.status(401).send("Un-Authorized User")
+          }
+         
+        })
+        .catch((error) => {
+          res.status(401).send("Un-Authorized User")
+          console.log(error)
+        });
+    }
+    else {
+      res.status(401).send("Un-Authorized User")
+    }
+
+  })
+
+
+
+
+
+
 
   // resister add 
 
@@ -83,19 +141,19 @@ app.get("/event/:id" , (req , res)=>{
   })
 
   // get data for resister from
-  app.get("/resisterData",(req , res)=>{
+  app.get("/resisterData", (req, res) => {
     resisterCollection.find()
-    .toArray((err, docs) => {
-      res.send(docs)
-    })
+      .toArray((err, docs) => {
+        res.send(docs)
+      })
   })
 
   // delete a single data from resister 
-  app.delete("/delete/:id",(req,res)=>{
-    resisterCollection.deleteOne({_id : ObjectId(req.params.id)})
-    .then(result=>{
-      res.send(result.deletedCount > 0)
-    })
+  app.delete("/delete/:id", (req, res) => {
+    resisterCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0)
+      })
   })
 
 });
